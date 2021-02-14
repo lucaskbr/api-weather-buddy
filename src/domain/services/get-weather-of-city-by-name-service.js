4dc1fec28ad455a709acead8aa3ceb66bbd92154
lucaskbr <lucas_klasa@hotmail.com>
2021-02-14 12:59:26 -0300

    feat: add get-weather-of-city-by-name-service

---
+++ src/domain/services/get-weather-of-city-by-name-service.js
@@ -0,0 +1,29 @@
+module.exports = class GetWeatherOfCityByNameService {
+  constructor ({
+    getCityWeatherByNameInCache,
+    getCityWeatherByNameInApi,
+    saveCityWeatherInCache
+  } = {}) {
+    this.getCityWeatherByNameInCache = getCityWeatherByNameInCache
+    this.getCityWeatherByNameInApi = getCityWeatherByNameInApi
+    this.saveCityWeatherInCache = saveCityWeatherInCache
+  }
+
+  async get (cityName) {
+    if (!cityName) {
+      throw new Error('Missing param: cityName')
+    }
+
+    const cachedCityWeather = await this.getCityWeatherByNameInCache.get(cityName)
+
+    if (cachedCityWeather) {
+      return cachedCityWeather
+    }
+
+    const cityWeather = await this.getCityWeatherByNameInApi.get(cityName)
+
+    this.saveCityWeatherInCache.save(cityWeather)
+
+    return cityWeather
+  }
+}
