fa0a51cbab334b90691cb9dfe5e78a2de5e724f5
lucaskbr <lucas_klasa@hotmail.com>
2021-02-16 15:39:56 -0300

    feat: change injection of weather-route-composite

--- src/config/routes/composites/weather-route-composite.js
+++ src/config/routes/composites/weather-route-composite.js
@@ -3,23 +3,23 @@ const GetWeatherByCityNameController = require('../../../adapters/web/controller
 
 const GetWeatherByCityNameService = require('../../../domain/services/get-weather-by-city-name-service')
 
-const GetWeatherByCityName = require('../../../adapters/open-weather-api/operations/get-weather-by-city-name')
+const GetWeatherByCityNameApi = require('../../../adapters/open-weather-api/operations/get-weather-by-city-name')
 
-const GetDataByKey = require('../../../adapters/cache/redis/get-data-by-key')
+const SaveWeatherByCityName = require('../../../adapters/cache/redis/weather/save-weather-by-city-name')
+const GetWeatherByCityNameRedis = require('../../../adapters/cache/redis/weather/get-weather-by-city-name')
 
 const httpClient = require('../../../adapters/open-weather-api/http-client')
 const redisClient = require('../../redis-client')
-const SaveDataByKey = require('../../../adapters/cache/redis/save-data-by-key')
 
 module.exports = class WeatherRouteComposite {
   static getByCityName (req, res) {
     return new GetWeatherByCityNameController({
       getWeatherByCityNameService: new GetWeatherByCityNameService({
-        getCityWeatherByNameInApi: new GetWeatherByCityName({
+        getCityWeatherByNameInApi: new GetWeatherByCityNameApi({
           httpClient: httpClient
         }),
-        getCityWeatherByNameInCache: new GetDataByKey({ redisClient }),
-        saveCityWeatherInCache: new SaveDataByKey({ redisClient })
+        getCityWeatherByNameInCache: new GetWeatherByCityNameRedis({ redisClient }),
+        saveCityWeatherInCache: new SaveWeatherByCityName({ redisClient })
       })
     })
       .handle(req, res)
