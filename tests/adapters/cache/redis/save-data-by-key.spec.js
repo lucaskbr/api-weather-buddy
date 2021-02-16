923cd699bcbbc367eab426753e670eaac4a10176
lucaskbr <lucas_klasa@hotmail.com>
2021-02-16 12:05:36 -0300

    test: save-data-by-key in redis cache

---
+++ tests/adapters/cache/redis/save-data-by-key.spec.js
@@ -0,0 +1,20 @@
+const SaveDataByKey = require('../../../../src/adapters/cache/redis/save-data-by-key')
+
+const makeSut = () => {
+  const sut = new SaveDataByKey({ redisClient: null })
+  return { sut }
+}
+
+describe('Adapters - Cache - Redis - Save Data By Key', () => {
+  it('Should throw an error if key param is not provided', async () => {
+    const { sut } = makeSut()
+    await expect(sut.save()).rejects
+      .toThrowError('Missing param: key')
+  })
+
+  it('Should throw an error if data param is not provided', async () => {
+    const { sut } = makeSut()
+    await expect(sut.save('key')).rejects
+      .toThrowError('Missing param: data')
+  })
+})
