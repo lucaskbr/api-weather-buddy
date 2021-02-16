27a1598f93168a50c1de3b60c808b03f4fc20d40
lucaskbr <lucas_klasa@hotmail.com>
2021-02-16 18:21:52 -0300

    refactor: change param name

--- src/adapters/web/controllers/weather/get-weather-by-city-name-controller.js
+++ src/adapters/web/controllers/weather/get-weather-by-city-name-controller.js
@@ -7,13 +7,13 @@ module.exports = class GetWeatherByCityNameController {
     try {
       const { cityName } = httpReq.params
 
-      const city = await this.getWeatherByCityNameService.get(cityName)
+      const cityWeather = await this.getWeatherByCityNameService.get(cityName)
 
-      if (!city) {
+      if (!cityWeather) {
         return httpRes.status(400).json({ message: 'City not found' })
       }
 
-      return httpRes.status(200).json(city)
+      return httpRes.status(200).json(cityWeather)
     } catch (e) {
       return httpRes.status(500)
     }
