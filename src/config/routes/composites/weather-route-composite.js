b688d39cea1d8dc2e94e401e3eedc3ffbca99d5e
lucaskbr <lucas_klasa@hotmail.com>
2021-02-16 18:29:01 -0300

    feat: add getLastSearched to weather-route-composite

--- src/config/routes/composites/weather-route-composite.js
+++ src/config/routes/composites/weather-route-composite.js
@@ -1,12 +1,13 @@
 
 const GetWeatherByCityNameController = require('../../../adapters/web/controllers/weather/get-weather-by-city-name-controller')
-
 const GetWeatherByCityNameService = require('../../../domain/services/get-weather-by-city-name-service')
-
 const GetWeatherByCityNameApi = require('../../../adapters/open-weather-api/operations/get-weather-by-city-name')
-
 const SaveWeatherByCityName = require('../../../adapters/cache/redis/weather/save-weather-by-city-name')
 const GetWeatherByCityNameRedis = require('../../../adapters/cache/redis/weather/get-weather-by-city-name')
+
+const GetWeatherOfAllLastSearchedCitiesController = require('../../../adapters/web/controllers/weather/get-weather-of-all-last-searched-cities-controller')
+const GetWeatherOfAllLastSearchedCitiesService = require('../../../domain/services/get-weather-of-all-last-searched-cities-service')
+const GetWeatherOfLastSearchedCities = require('../../../adapters/cache/redis/weather/get-weather-of-last-searched-cities')
 
 const httpClient = require('../../../adapters/open-weather-api/http-client')
 const redisClient = require('../../redis-client')
@@ -21,7 +22,16 @@ module.exports = class WeatherRouteComposite {
         getCityWeatherByNameInCache: new GetWeatherByCityNameRedis({ redisClient }),
         saveCityWeatherInCache: new SaveWeatherByCityName({ redisClient })
       })
-    })
-      .handle(req, res)
+    }).handle(req, res)
+  }
+
+  static getLastSearched (req, res) {
+    return new GetWeatherOfAllLastSearchedCitiesController({
+      getWeatherOfAllLastSearchedCitiesService: new GetWeatherOfAllLastSearchedCitiesService({
+        getLastSearchedCities: new GetWeatherOfLastSearchedCities({
+          redisClient
+        })
+      })
+    }).handle(req, res)
   }
 }
