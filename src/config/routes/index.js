f3356c208e9119668bdbd958edcd21570d2bd141
lucaskbr <lucas_klasa@hotmail.com>
2021-02-16 18:29:25 -0300

    feat: add get /weather route

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -6,4 +6,6 @@ const router = Router()
 
 router.get('/weather/:cityName', WeatherRouteComposite.getByCityName)
 
+router.get('/weather', WeatherRouteComposite.getLastSearched)
+
 module.exports = router
